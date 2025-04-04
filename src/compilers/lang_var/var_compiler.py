e5abfe548d30eb5d5eea0aa24059c4fa32b3aa23
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2025-04-04 10:57:35 +0200

    Finished Sheet 1

--- src/compilers/lang_var/var_compiler.py
+++ src/compilers/lang_var/var_compiler.py
@@ -9,7 +9,7 @@ def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
     Compiles the given module.
     """
     vars = var_tychecker.tycheckModule(m)
-    instrs = compileStmts(m.stmts)
+    instrs = compileStmts(m.stmts, vars)
     idMain = WasmId('$main')
     locals: list[tuple[WasmId, WasmValtype]] = [(identToWasmId(x), 'i64') for x in vars]
     return WasmModule(imports=wasmImports(cfg.maxMemSize),
@@ -19,47 +19,70 @@ def compileModule(m: mod, cfg: CompilerConfig) -> WasmModule:
                     funcTable=WasmFuncTable([]),
                     funcs=[WasmFunc(idMain, [], None, locals, instrs)])
 
-def compileStmts(stmts: list[stmt]) -> list[WasmInstr]:
-    instr1 = WasmInstrConst("i64", 42)
-    instrcall = WasmInstrCall(WasmId("$print_i64"))
-    return [instr1, instrcall]
-    # for stmt in stmts:
-    #     instrs.append(WasmInstrCall(identToWasmId(Ident("print"))))
-    # return instrs
+def compileStmts(stmts: list[stmt], vars:set[ident]) -> list[WasmInstr]:
+    wasmInstrs:list[WasmInstr] = []
 
-def compileStmt(s: stmt) -> list[WasmInstr]:
+    for stmt in stmts:
+        wasmInstrs.extend(compileStmt(stmt, vars))
+
+    return wasmInstrs
+
+def compileStmt(s: stmt, vars:set[ident]) -> list[WasmInstr]:
     instrs:list[WasmInstr] = []
     match s:
         case StmtExp():
             print(f'Compiling statement: {s}')
-            compileExp(s.exp, instrs)
-        case Assign(x, IntConst(n)):
-            instrs.append(WasmInstrConst("i64", n))
-            instrs.append(WasmInstrCall(identToWasmId(x)))
+            var_tychecker.tycheckStmt(s, vars)
+            instrs.extend(compileExp(s.exp))
+        case Assign(var, right):
             print(f'Compiling assignment: {s}')
-        case Assign(_,_):
-            print(f'Compiling assignment: {s}')
+            var_tychecker.tycheckStmt(s, vars)
+            instrs.extend(compileAssign(var, right))
 
     return instrs
 
-def compileAssign(var: ident, right: exp):
-    return compileExp(right)
 
 
-def compileExp(s: exp, instrs: list[WasmInstr]):
+def compileExp(s: exp) -> list[WasmInstr]:
     """
     Compiles an expression statement.
     """
+    wasmInstrs:list[WasmInstr] = []
     match s:
         case IntConst(n):
-            instrs.append(WasmInstrConst("i64", n))
+            wasmInstrs.append(WasmInstrConst("i64", n))
         case Name(x):
-            instrs.append(WasmInstrVarLocal("get", identToWasmId(x)))
+            wasmInstrs.append(WasmInstrVarLocal("get", identToWasmId(x)))
         case Call(id, args):
+            # WasmInstrCall
+            for arg in args:
+                wasmInstrs.extend(compileExp(arg))
+            wasmInstrs.append(WasmInstrCall(WasmId(f'${id.name.split("_")[0]}_i64')))
+        case BinOp(left, op, right):
+            # WasmInstrNumBinOp
+            wasmInstrs.extend(compileExp(left))
+            wasmInstrs.extend(compileExp(right))
+            
+            match op:
+                case Add():
+                    wasmInstrs.append(WasmInstrNumBinOp("i64", "add"))
+                case Sub():
+                    wasmInstrs.append(WasmInstrNumBinOp('i64', 'sub'))
+                case Mul():
+                    wasmInstrs.append(WasmInstrNumBinOp('i64', 'mul'))
 
-        case _:
-            raise CompileError.typeError(f'Unsupported expression type: {type(s.exp)}')
+        case UnOp(op, e):
+            wasmInstrs.extend(compileExp(e))
+            wasmInstrs.append(WasmInstrConst('i64', -1))
+            wasmInstrs.append(WasmInstrNumBinOp('i64', 'mul'))
 
+    return wasmInstrs
+
+def compileAssign(var: ident, right: exp) -> list[WasmInstr]:
+    wasmInstrs:list[WasmInstr] = []
+    wasmInstrs.extend(compileExp(right))
+    wasmInstrs.append(WasmInstrVarLocal("set", identToWasmId(var)))
+    return wasmInstrs
 
 def identToWasmId(i: ident) -> WasmId:
     """
